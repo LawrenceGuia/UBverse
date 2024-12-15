ca45c68c6bfc1cedf87ac38bb9082a00f1608213
LawrenceGuia <1600012@ub.edu.ph>
2024-12-15 21:10:35 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './/Users/USER/OneDrive/Documents/UBverse/App'; // or wherever your main app component is
+import { name as appName } from './UBverse.json'; // This is important
+
+AppRegistry.registerComponent(appName, () => App);
